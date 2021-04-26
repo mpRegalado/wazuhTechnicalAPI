f8be400675304cf207bf523fa5ce5e38ccefabc8
Manuel Gomez <mpr.gomezc@gmail.com>
2021-04-26 16:38:04 +0200

    Removed flask_cors

--- endpoints.py
+++ endpoints.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, jsonify, request
 import pdb
-from flask_cors import cross_origin
 
 def corsDecorator(func):
     def wrapper(*args, **kwargs):
@@ -13,7 +12,6 @@ def create_blueprint_data(parser):
     endpoint = Blueprint("access_data",__name__)
 
     @endpoint.route("/alerts", methods=["GET"])
-    @cross_origin(origin='*',headers=['Content-Type','Authorization'])
     def get_alerts():
         offset = int(request.args.get("offset"))
         limit = int(request.args.get("limit"))
@@ -23,37 +21,38 @@ def create_blueprint_data(parser):
         else:
             id = []
 
-        response = parser.get_alerts(offset,limit,id)
-        return jsonify(response)
+        response = jsonify(parser.get_alerts(offset,limit,id))
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
     
     @endpoint.route("/agents", methods=["GET"])
-    @cross_origin(origin='*',headers=['Content-Type','Authorization'])
     def get_agents():
         offset = int(request.args.get("offset"))
         limit = int(request.args.get("limit"))
 
-        response = parser.get_agents(offset,limit)
-        return jsonify(response)
+        response = jsonify(parser.get_agents(offset,limit))
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
     
     @endpoint.route("/agents/:<int:id>")
-    @cross_origin(origin='*',headers=['Content-Type','Authorization'])
     def get_agent(id):
-        response = parser.get_agent_by(id)
+        jsonify(response = parser.get_agent_by(id))
 
-        return jsonify(response)
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
 
     @endpoint.route("/rules")
-    @cross_origin(origin='*',headers=['Content-Type','Authorization'])
     def get_rules():
         offset = int(request.args.get("offset"))
         limit = int(request.args.get("limit"))
-        response = parser.get_rules(offset,limit)
-        return jsonify(response)
+        response = jsonify(parser.get_rules(offset,limit))
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
 
     @endpoint.route("/rules/:<int:id>")
-    @cross_origin(origin='*',headers=['Content-Type','Authorization'])
     def get_rule(id):
-        response = parser.get_rule_by(id)
-        return jsonify(response)
+        response = jsonify(parser.get_rule_by(id))
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
 
     return endpoint
