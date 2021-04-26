ebeaabefe96c4d3432c95db3979a736733daf7bc
Manuel Gomez <mpr.gomezc@gmail.com>
2021-04-26 15:01:13 +0200

    Fixed CORS

--- endpoints.py
+++ endpoints.py
@@ -1,10 +1,17 @@
 from flask import Blueprint, jsonify, request
 import pdb
 
+def corsDecorator(func):
+    def wrapper(*args, **kwargs):
+        response = func(*args, **kwargs)
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
+    return wrapper
 
 def create_blueprint_data(parser):
     endpoint = Blueprint("access_data",__name__)
 
+    @corsDecorator
     @endpoint.route("/alerts", methods=["GET"])
     def get_alerts():
         offset = int(request.args.get("offset"))
@@ -18,6 +25,7 @@ def create_blueprint_data(parser):
         response = parser.get_alerts(offset,limit,id)
         return jsonify(response)
     
+    @corsDecorator
     @endpoint.route("/agents", methods=["GET"])
     def get_agents():
         offset = int(request.args.get("offset"))
@@ -26,12 +34,14 @@ def create_blueprint_data(parser):
         response = parser.get_agents(offset,limit)
         return jsonify(response)
     
+    @corsDecorator
     @endpoint.route("/agents/:<int:id>")
     def get_agent(id):
         response = parser.get_agent_by(id)
 
         return jsonify(response)
 
+    @corsDecorator
     @endpoint.route("/rules")
     def get_rules():
         offset = int(request.args.get("offset"))
@@ -39,6 +49,7 @@ def create_blueprint_data(parser):
         response = parser.get_rules(offset,limit)
         return jsonify(response)
 
+    @corsDecorator
     @endpoint.route("/rules/:<int:id>")
     def get_rule(id):
         response = parser.get_rule_by(id)
